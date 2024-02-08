debf677862473942ad9f7cca69280e0d8f1f3711
sudokar <sudokar@yahoo.com>
2024-02-08 17:59:41 +0000

    Fixed jest config gen issue

--- tools/workspace-plugin/src/generators/serverless/jest-config.ts
+++ tools/workspace-plugin/src/generators/serverless/jest-config.ts
@@ -1,16 +1,15 @@
 import { Tree } from '@nx/devkit';
-import { jestProjectGenerator } from '@nx/jest';
-import { JestProjectSchema } from '@nx/jest/src/generators/jest-project/schema';
+import { configurationGenerator } from '@nx/jest';
+import { JestProjectSchema } from '@nx/jest/src/generators/configuration/schema';
 
 export const addJest = async (host: Tree, projectName: string) => {
-  await jestProjectGenerator(host, <JestProjectSchema>{
+  await configurationGenerator(host, <JestProjectSchema>{
     project: projectName,
     setupFile: 'none',
     testEnvironment: 'node',
     skipSerializers: false,
-    skipSetupFile: false,
     supportTsx: false,
-    babelJest: false,
+    compiler: 'swc',
     skipFormat: true,
   });
 };
